ec571ec4b22c7e42a35a5141f69c9ac89002518f
Katrina Moody <kmfoster212@gmail.com>
2025-04-30 20:27:47 -0400

    Completed basic styling. Working on game logic.

---
+++ script.js
@@ -0,0 +1,24 @@
+const playerOne = {
+        name: "Player 1",
+        marker: "x"
+    }
+
+    const playerTwo = {
+        name: "Player 2",
+        marker: "o"
+    }
+    
+function playGame() {
+    player = this.player;
+    marker = this.marker;
+
+    
+
+
+function gameOver(winningPlayer){
+    console.log(winningPlayer.name + " wins!")
+}
+
+}
+
+
