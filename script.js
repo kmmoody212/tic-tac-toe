0f773d91fb78daf2c4dfc779102bb78ae4a97505
Katrina Moody <kmfoster212@gmail.com>
2025-05-12 17:20:19 -0400

    Can now see which player's turn it is, and a congratulations message for the winner. Need to change alert to a modal!

--- script.js
+++ script.js
@@ -3,40 +3,40 @@
 let gameboard = (function() {
     let board = [];
     const container = document.querySelector(".board"); // need container to append divs to
-
     for (let i = 0; i < 3; i++) {
         board[i] = [];
         for (let j = 0; j < 3; j++) {
             let square = document.createElement("div"); // need to set created element to a variable
             square.classList.add("square"); // give the created div a class name
             square.addEventListener("click", function() {
-                console.log(`Square clicked at row ${i} and column ${j}`);
+                game.playerTurnMessage();
                 updateBoard(i, j);
             })
             container.appendChild(square); // need to append each div so it shows in window
             board[i][j] = square; // now each space in a 2d array is attached to a "square" (div)
-        
         }
     }
     
-    // Method to show the board
+    // Method to show the board - for outside variables to access
     function getBoard() {
     return board;
     }
     
     // Method to update the board
     function updateBoard(i, j) {
-    game.checkPlayerTurn();    
-    let marker = currentPlayer.marker;
-    if (board[i][j].textContent === '') {
-        board[i][j].textContent = marker;
-        if(checkForWin()) {
-            console.log(`CONGRATS ${currentPlayer.name}! YOU WIN!`);
-            alert(`RESTART GAME?`);
-            resetBoard();}
+    game.checkPlayerTurn(); // first, check which player's turn it is
+    let marker = currentPlayer.marker; // marker to be placed belongs to the current player
+    if (board[i][j].textContent === '') { // if the content in the target square is blank - 
+        board[i][j].textContent = marker; // set the content to player's marker
+        if(checkForWin()) { // and if checkForWin() comes back true, do the following:
+            game.winnerMessage();
+            // alert(`RESTART GAME?`);
+            resetBoard();
+        }
             
          else {
             game.nextTurn();
+            game.playerTurnMessage();
         }} else {
             console.log(`THAT SLOT IS ALREADY TAKEN! TRY AGAIN`)}
     
@@ -64,6 +64,8 @@ let gameboard = (function() {
         } 
         return false;
     }
+
+
 
     // Method to reset each space in the board
     function resetBoard() {
@@ -101,9 +103,28 @@ let game = (function() {
     function start() {
         turn = 1;
         console.log(turn)
+        playerTurnMessage();
         return gameboard.getBoard();
 
     }
+
+    function playerTurnMessage() {
+        let whosTurn = document.querySelector(".player-turn");
+        checkPlayerTurn();
+        if(turn % 2 === 0 ) {
+            whosTurn.textContent = `IT IS PLAYER 2'S TURN`
+        } else {
+            whosTurn.textContent = `IT IS PLAYER 1'S TURN`
+        }
+        
+    }
+
+    function winnerMessage() {
+        let msg = document.querySelector(".modal")
+        return msg.textContent = `CONGRATS ${currentPlayer.name}! YOU WIN!!!`
+    }
+
+
     // Method to check which player's turn it is
     function checkPlayerTurn() {
         if (turn % 2 === 0) {
@@ -128,6 +149,8 @@ let game = (function() {
 
     return {
         start,
+        playerTurnMessage,
+        winnerMessage,
         checkPlayerTurn,
         nextTurn,
     }
