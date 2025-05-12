63ab8d64764a55cdb94f7f4415be39b97c7bba35
Katrina Moody <kmfoster212@gmail.com>
2025-05-12 14:57:39 -0400

    Each square now updates with player marker when clicked on. Need to rework game reset function.

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ let gameboard = (function() {
         for (let j = 0; j < 3; j++) {
             let square = document.createElement("div"); // need to set created element to a variable
             square.classList.add("square"); // give the created div a class name
+            square.addEventListener("click", function() {
+                console.log(`Square clicked at row ${i} and column ${j}`);
+                updateBoard(i, j);
+            })
             container.appendChild(square); // need to append each div so it shows in window
             board[i][j] = square; // now each space in a 2d array is attached to a "square" (div)
         
@@ -19,13 +23,13 @@ let gameboard = (function() {
     function getBoard() {
     return board;
     }
-
+    
     // Method to update the board
     function updateBoard(i, j) {
     game.checkPlayerTurn();    
     let marker = currentPlayer.marker;
-    if (board[i][j] === '') {
-        board[i][j] = marker;
+    if (board[i][j].textContent === '') {
+        board[i][j].textContent = marker;
         if(checkForWin()) {
             console.log(`CONGRATS ${currentPlayer.name}! YOU WIN!`);
             resetBoard();
@@ -40,34 +44,25 @@ let gameboard = (function() {
     function checkForWin() {
         // check through rows
         for (let row = 0; row < 3; row++) {
-            if (board[row][0] === currentPlayer.marker && board[row][1] === currentPlayer.marker && board[row][2] === currentPlayer.marker) {
+            if (board[row][0].textContent === currentPlayer.marker && board[row][1].textContent === currentPlayer.marker && board[row][2].textContent === currentPlayer.marker) {
                 return true;
             } 
         }
         // check through columns
         for (let col = 0; col < 3; col++) {
-            if (board[0][col] === currentPlayer.marker && board[1][col] === currentPlayer.marker && board[2][col] === currentPlayer.marker) {
+            if (board[0][col].textContent === currentPlayer.marker && board[1][col].textContent === currentPlayer.marker && board[2][col].textContent === currentPlayer.marker) {
                 return true;
             }
         }
         // check diagonals
-        if ((board[0][0] === currentPlayer.marker && board[1][1] === currentPlayer.marker && board[2][2] === currentPlayer.marker) 
-         || (board[2][0] === currentPlayer.marker && board[1][1] === currentPlayer.marker && board[0][2] === currentPlayer.marker)) {
+        if ((board[0][0].textContent === currentPlayer.marker && board[1][1].textContent === currentPlayer.marker && board[2][2].textContent === currentPlayer.marker) 
+         || (board[2][0].textContent === currentPlayer.marker && board[1][1].textContent === currentPlayer.marker && board[0][2].textContent === currentPlayer.marker)) {
             return true;
         } 
         return false;
     }
     // Method to reset each space in the board to 0
     function resetBoard() {
-    board[0][0] = 0;
-    board[0][1] = 0;
-    board[0][2] = 0;
-    board[1][0] = 0;
-    board[1][1] = 0;
-    board[1][2] = 0;
-    board[2][0] = 0;
-    board[2][1] = 0;
-    board[2][2] = 0;
     game.start()
     
     }
@@ -87,7 +82,7 @@ let gameboard = (function() {
 function player(name, marker) {
     return { name, marker }
 };
-const player1 = player('PLAYER', 'X');
+const player1 = player('PLAYER 1', 'X');
 const player2 = player('PLAYER 2', 'O');
 
 
@@ -96,7 +91,7 @@ let game = (function() {
     let turn = 0;
     function start() {
         turn = 1;
-        
+        console.log(turn)
         return gameboard.getBoard();
 
     }
@@ -115,7 +110,8 @@ let game = (function() {
             console.log(`IT'S A TIE!`);
             return gameboard.resetBoard();
         } else {
-            return turn += 1;
+            turn += 1;
+            return console.log(turn)
         }
         
     }
@@ -125,7 +121,7 @@ let game = (function() {
         checkPlayerTurn,
         nextTurn,
     }
-}());
+})();
 
 // /////////////////////////////// STARTS THE GAME RIGHT AWAY //////////////////////
 game.start();
